fb70fa34e3060b275ac946ae040f8c68f786c729
Steven Hook <steven@hookscode.com>
2018-01-27 13:15:02 -0500

    Generate floating enemies based on probabilities

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Erithroblast/Enemy")]
+public class Enemy : ScriptableObject
+{
+    public float ProbabilityOfSpawn;
+    public GameObject Prefab;
+}
---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public Transform HorizontalLocationOfSpawnPoint;
+    public Transform TopOfSpawnPoint;
+    public Transform BottomOfSpawnPoint;
+    public float SpawnTime = 3f;
+    public Enemy[] Enemies;
+    
+    void Start()
+    {
+        InvokeRepeating("Spawn", SpawnTime, SpawnTime);
+    }
+
+    void Spawn()
+    {
+        var enemy = GetNextEnemy();
+        if (enemy == null)
+            return;
+
+        var newPosition = new Vector2(HorizontalLocationOfSpawnPoint.position.x, Random.Range(BottomOfSpawnPoint.position.y, TopOfSpawnPoint.position.y));
+
+        Instantiate(enemy.Prefab, newPosition, Quaternion.LookRotation(Vector3.forward));
+    }
+
+    private Enemy GetNextEnemy()
+    {
+        var randomRoll = Random.Range(0f, 1f);
+        float totalPercentage = 0f;
+
+        Enemy nextEnemy = null;
+        foreach (var enemy in Enemies)
+        {
+            totalPercentage += enemy.ProbabilityOfSpawn;
+            if (randomRoll <= totalPercentage)
+            {
+                nextEnemy = enemy;
+                break;
+            }
+        }
+
+        return nextEnemy;
+    }
+}
