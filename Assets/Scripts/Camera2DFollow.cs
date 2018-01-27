bfef99c19b5c5e1336346225c61c55da15e0543a
Steven Hook <steven@hookscode.com>
2018-01-27 00:59:49 -0500

    Add chase camera and prefab

---
+++ Assets/Scripts/Camera2DFollow.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera2DFollow : MonoBehaviour {
+
+    public string TagNameToFollow = "Player";
+    public float Damping = 0.5f;
+    public Transform StartOfLevel;
+    public Transform EndOfLevel;
+    public Transform TopOfLevel;
+    public Transform BottomOfLevel;
+
+    private Transform[] _targets;
+    private int _targetLength;
+    private Vector3 _currentVelocity;
+    private float _startOfLevelX;
+    private float _endOfLevelX;
+    private float _topOfLevelY;
+    private float _bottomOfLevelY;
+
+    // Use this for initialization
+    void Start()
+    {
+        _startOfLevelX = StartOfLevel.position.x;
+        _endOfLevelX = EndOfLevel.position.x;
+        _topOfLevelY = TopOfLevel.position.y;
+        _bottomOfLevelY = BottomOfLevel.position.y;
+        LookForTargets();
+    }
+
+    void Update()
+    {
+        if (_targets == null || _targets.Length == 0)
+        {
+            LookForTargets();
+            return;
+        }
+
+        float minX = _targets[0].position.x;
+        float maxX = minX;
+
+        float minY = _targets[0].position.y;
+        float maxY = minY;
+
+        for (int i = 1; i < _targetLength; i++)
+        {
+            var currentX = _targets[i].position.x;
+
+            if (currentX > maxX)
+                maxX = currentX;
+
+            if (currentX < minX)
+                minX = currentX;
+        }
+
+        for (int i = 1; i < _targetLength; i++)
+        {
+            var currentY = _targets[i].position.y;
+
+            if (currentY > maxY)
+                maxY = currentY;
+
+            if (currentY < minY)
+                minY = currentY;
+        }
+
+        var currentPosition = transform.position;
+        var distanceBetweenPlayers = maxX - minX;
+        var newX = Mathf.Clamp(minX + (distanceBetweenPlayers / 2f), _startOfLevelX, _endOfLevelX);
+        var newY = Mathf.Clamp(minY + (distanceBetweenPlayers / 2f), _bottomOfLevelY, _topOfLevelY);
+
+        var targetPosition = new Vector3(newX, newY, currentPosition.z);
+        var newPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref _currentVelocity, Damping);
+
+        transform.position = newPosition;
+    }
+
+    private void LookForTargets()
+    {
+        var targetObjects = GameObject.FindGameObjectsWithTag(TagNameToFollow);
+        _targetLength = targetObjects.Length;
+        _targets = new Transform[_targetLength];
+        for (int i = 0; i < _targetLength; i++)
+        {
+            _targets[i] = targetObjects[i].transform;
+        }
+    }
+}
