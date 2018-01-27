77142c954836334e99608a3cf7ca7ae491e4711d
Steven Hook <steven@hookscode.com>
2018-01-27 17:51:24 -0500

    Add spike enemy and the start of projectiles

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] Launchers;
+    public float AttackRating = 1f;
+    public GameObject ProjectilePrefab;
+
+    void Start()
+    {
+        InvokeRepeating("Fire", AttackRating, AttackRating);
+    }
+
+    void Fire()
+    {
+        foreach (var launcher in Launchers)
+        {
+            Instantiate(ProjectilePrefab, launcher.position, launcher.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float Speed = 2f;
+
+    private Rigidbody2D _rigidbody;
+
+    public void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidbody.velocity = new Vector2(Speed, 0);
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            // do stuff
+            Destroy(gameObject);
+        }
+    }
+
+    public void FixedUpdate()
+    {
+        _rigidbody.AddForce(transform.forward * Speed);
+        //_rigidbody.velocity = new Vector2(Speed, 0);
+    }
+}
