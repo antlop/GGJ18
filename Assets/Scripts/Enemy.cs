1242b3f608f0d9bf90690e3ce7fb501a6c30f4a5
Steven Hook <steven@hookscode.com>
2018-01-27 14:55:34 -0500

    Create friend spawner from enemy spawner

--- Assets/Scripts/Enemy.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-[CreateAssetMenu(menuName = "Erithroblast/Enemy")]
-public class Enemy : ScriptableObject
-{
-    public float ProbabilityOfSpawn;
-    public GameObject Prefab;
-}
---
+++ Assets/Scripts/FriendSpawn.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Erythroblast/FriendSpawn")]
+public class FriendSpawn : SpawnableObject
+{
+}
